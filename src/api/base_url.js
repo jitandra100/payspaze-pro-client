8cea0c114e47074ba4f230592bbf0ab306bd6943
jitandra100 <chauhanlearninghub@gmail.com>
2024-12-12 12:13:53 +0530

    Update base_url.js

--- src/api/base_url.js
+++ src/api/base_url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://backend-data-neuron-assessment-production.up.railway.app'
-export const BASE_URL = 'http://localhost:5005/v1'
+export const BASE_URL = 'https://payspaze-pro-server-production.up.railway.app/v1'
+// export const BASE_URL = 'http://localhost:5005/v1'
